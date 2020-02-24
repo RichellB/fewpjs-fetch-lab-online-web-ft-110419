5f0279ee1f044f2bc8eb81c5726b299f105a52fe
Richell Bonfield <richell.bonfield12@gmail.com>
2020-02-24 05:59:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 function fetchBooks() {
   
   fetch('https://anapioficeandfire.com/api/books')
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(json) {
-    console.log(json)
-  });
+  .then(resp => resp.json())
+  .then(json => console.log(json));
+  
+  
 
 }
 
